001e3103953add485805a1c0b5d1afbea9bd740c
Catherine Lin <Catherine@Catherines-MacBook-Pro.local>
2016-05-15 20:14:50 -0700

    cpp files with doxygen

--- generate.cpp
+++ generate.cpp
@@ -1,3 +1,23 @@
+/*! 
+ @file generate.cpp
+ @brief generate a file with CPU usage of two CPUs of 1000 servers every minute of 2014-10-31
+ @details Change of date will require changes to START_TIME, END_TIME and DATE strings. Assuming our servers' IP addresses are 192.168.1.0 ~ 192.168.1.249, 192.168.2.0 ~ 192.168.2.249, 192.168.3.0 ~ 192.168.3.249. That's in total 4 * 250 = 1000 servers.
+ @author Yilei Zhang
+ @date 2016-5-15
+ @param destination path and file name fo the file to be generated
+ @param f an output stream to destination
+ @param to_print the string to put into the destination
+*/
+
+/*!
+ @mainpage
+ \verbatim
+    generate.sh generates a file of 2,880,000 entries of foramt:
+        > 1414713600 192.168.1.0 1 30
+ which corresponds to
+        > timestamp  ip        cpu  usage
+ \endverbatim
+ */
 
 #include <iostream>
 #include <string>
@@ -5,30 +25,47 @@
 #include <stdlib.h>
 #include <time.h>
 
+/// prefix of the first 250 IP addresses
 const std::string COMMON_IP_ONE = "192.168.1.";
+
+/// prefix of the second 250 IP addresses
 const std::string COMMON_IP_TWO = "192.168.2.";
+
+/// prefix of the third 250 IP addresses
 const std::string COMMON_IP_THREE = "192.168.3.";
+
+/// prefix of the last 250 IP addresses
 const std::string COMMON_IP_FOUR = "192.168.4.";
 
+/// timestamp of 2014-10-31 00:00:00
 const int START_TIME = 1414713600;
-const int END_TIME = 1414800000;
 
-int main(int argc, const char * argv[]) {
-    if(argc != 2){
-        return -1;
-    }
+/// timestamp of 2014-10-31 23:59:59
+const int END_TIME = 1414799999;
 
-    std::string destination(argv[1]);
-    if((argv[1][sizeof(argv[1]) - 1]) != '/'){
+/// name of the file to be generated
+const std::string DATE = "2014-10-31";
+
+/*!
+ takes a uncompleted path, which might end with / or /., and convert it to a full path, including adding 2014-10-31 to the file name
+ @param argv is a pointer to an array of char (which is what the user specifies as path)
+ @return the modified path
+*/
+std::string toPath(const char *argv){
+    std::string destination(argv);
+    if((argv[sizeof(argv) - 1]) != '/'){
         destination += "/";
     }
-    destination += "2014-10-31";
-    std::fstream f(destination, std::fstream::out);
-    
-    srand(time(nullptr));
-    
+    destination += DATE;
+    return destination;
+}
+
+/*!
+ Uses global constants to generate a huge string to print.
+ @return string to print
+*/
+std::string printGen(){
     std::string toPrint;
-    
     int cur = START_TIME;
     while(cur < END_TIME){
         for(int i = 0; i < 250; i++){
@@ -53,7 +90,24 @@ int main(int argc, const char * argv[]) {
         }
         cur += 60;
     }
-    f << toPrint;
+    return toPrint;
+}
+
+int main(int argc, const char * argv[]) {
+    if(argc != 2){
+        std::cerr << "Wrong number of arguments." << std::endl;
+        return -1;
+    }
+    
+    std::string destination = toPath(argv[1]);
+    
+    std::fstream f(destination, std::fstream::out);
+    
+    srand(time(nullptr));
+    
+    std::string to_print = printGen();
+    
+    f << to_print;
     
     return 0;
     
--- query.cpp
+++ query.cpp
@@ -1,10 +1,19 @@
-//
-//  main.cpp
-//  Query
-//
-//  Created by Yilei Zhang on 5/12/16.
-//  Copyright © 2016 yilei. All rights reserved.
-//
+/*!
+ @file query.cpp
+ @brief Given a start time and an end time, with server IP address and CPU number, gives back the CPU usage of each minute in this time period by doing a search on the file generated.
+ @author Yilei Zhang
+ @date 2016-5-15
+ @param cpu_map a map with cpu as key and ip_map as value
+ @param time_lower lower time limit of user input
+ @param time_upper upper time limit of user input
+ @param cpu cpu number of user input
+ @param ip ip address of user input
+ */
+
+/*!
+ @mainpage
+ query.sh handles user's search of CPU usage with specific IP address,   CPU number, start and end time by optimizing the search by constructing threelayers of map.
+ */
 
 #include <iostream>
 #include <string>
@@ -17,13 +26,21 @@
 #include <time.h>
 #include <map>
 
-/*Three layers of map, each with a key to optimize search.
- The keys are cpu number, ip address, and the timestamp with timestamp being the inner key, and cpu being the outer key. */
+///maps cpu to ip map
 typedef std::map<unsigned long, int> InnerMap;
+
+///maps ip to timestamp map
 typedef std::map<std::string, InnerMap> MiddleMap;
+
+///maps timestamp to cpu usage
 typedef std::map<int, MiddleMap> OuterMap;
 
-/* This function split a strings into a set of strings by a string specified */
+/*!
+ This function splits a strings into a set of strings by a string specified
+ @param str the string to be separated
+ @param delimiter str is separated by delimiter
+ @return strings the vector to store separated strings
+ */
 std::vector<std::string> split_string(const std::string& str,
                                       const std::string& delimiter)
 {
@@ -43,6 +60,11 @@ std::vector<std::string> split_string(const std::string& str,
     return strings;
 }
 
+/*!
+ This function checks the format of date. A example of a correct date format is 2014-10-31
+ @param s the string that contains date
+ @return whether or not the date is of the right format
+ */
 bool check_date_format(std::string s){
     if(s.length() != 10){
         return false;
@@ -64,7 +86,11 @@ bool check_date_format(std::string s){
     return true;
 }
 
-
+/*!
+ This function checks the format of ip. A example of a correct date ip is 192.168.1.0.
+ @param s the string that contains ip
+ @return whether or not the ip is of the right format
+ */
 bool check_IP_format(std::string ip){
     std::vector<std::string> nums;
     nums = split_string(ip, ".");
@@ -83,6 +109,11 @@ bool check_IP_format(std::string ip){
     return true;
 }
 
+/*!
+ This function checks the format of time. A example of a correct date format is 00:00.
+ @param s the string that contains time
+ @return whether or not the time is of the right format
+ */
 bool check_time_format(std::string t){
     if ((t.length()) != 5){
         return false;
@@ -98,6 +129,157 @@ bool check_time_format(std::string t){
     return true;
 }
 
+/*!
+ This function sets up a map of all cpu usage report files. The outermost map is a cpu_map with cpu number as key, and ip_map as value. The middle map is a map with ip as key, and ts_map with timestamp as key, and cpu usage as value.
+ @param ifs input stream class of the current file
+ @param cpu_map the map to be set up, notice all files share the same map
+ */
+void mapSetup(std::string full_file_name, OuterMap &cpu_map){
+    std::ifstream ifs(full_file_name.c_str());
+    std::string line;
+    while (std::getline(ifs, line)){
+        if(ifs.eof()){
+            break;
+        }
+        std::vector<std::string> strings;
+        strings = split_string(line, " ");
+        unsigned long timestamp;
+        timestamp = stoul(strings[0]);
+        std::string ip = strings[1];
+        int cpu;
+        cpu = stoi(strings[2]);
+        int usage;
+        usage = stoi(strings[3]);
+        
+        OuterMap::iterator out = cpu_map.find(cpu);
+        
+        if (out != cpu_map.end())
+        {
+            MiddleMap::iterator mid = out->second.find(ip);
+            if (mid != out->second.end())
+            {
+                mid->second[timestamp] = usage;
+            }
+            else
+            {
+                InnerMap ts_map;
+                ts_map[timestamp] = usage;
+                out->second[ip] = ts_map;
+            }
+        }
+        else
+        {
+            InnerMap ts_map;
+            ts_map[timestamp] = usage;
+            MiddleMap ip_map;
+            ip_map[ip] = ts_map;
+            cpu_map[cpu] = ip_map;
+        }
+        
+        line.clear();
+    }
+    
+}
+
+
+/*!
+ This function parses user's command. It converts the start time and end time to timestamps, so that we can check whether or not the keys of ts_map are within bounds. It also sets up the right cpu_map key and ip_map key.
+ @param strings stores the strings separted by split_string function.
+ @param time_lower start time in timestamp format
+ @param time_upper end time in timestamp format
+ @param cpu user input cpu, which is also the key of cpu_map
+ @ip ip user input ip, which is the key of ip_map
+ */
+void commandParse(std::vector<std::string> strings, unsigned long &time_lower, unsigned long &time_upper, int &cpu, std::string ip){
+    
+    //assistant structure to record user input, and to store output info
+    struct tm time_info;
+    std::string year = strings[3].substr(0,4);
+    std::string mon = strings[3].substr(5,2);
+    std::string mday = strings[3].substr(8,2);
+    std::string hour = strings[4].substr(0,2);
+    std::string min = strings[4].substr(3,2);
+    
+    time_info.tm_year = stoi(year);
+    time_info.tm_year = time_info.tm_year - 1900;
+    time_info.tm_mon = stoi(mon);
+    time_info.tm_mon = time_info.tm_mon - 1;
+    time_info.tm_mday = stoi(mday);
+    time_info.tm_hour = stoi(hour);
+    time_info.tm_min = stoi(min);
+    time_info.tm_sec = 0;
+    
+    time_lower = static_cast<unsigned long>(timegm(&time_info));
+    
+    year = strings[5].substr(0,4);
+    mon = strings[5].substr(5,2);
+    mday = strings[5].substr(8,2);
+    hour = strings[6].substr(0,2);
+    min = strings[6].substr(3,2);
+    
+    time_info.tm_year = stoi(year);
+    time_info.tm_year = time_info.tm_year - 1900;
+    time_info.tm_mon = stoi(mon);
+    time_info.tm_mon = time_info.tm_mon - 1;
+    time_info.tm_mday = stoi(mday);
+    time_info.tm_hour = stoi(hour);
+    time_info.tm_min = stoi(min);
+    
+    time_upper = static_cast<unsigned long>(timegm(&time_info));
+    
+    cpu = stoi(strings[2]);
+    ip = strings[1];
+}
+
+/*!
+ This function puts the search result in a string.
+ @param result stores the search result
+ @param found a boolean variable to indicate whether or not a result is found
+ @param time_lower start time in timestamp format
+ @param time_upper end time in timestamp format
+ @param cpu_map nested map constructed to map cpu, ip and timestamp to CPU usage.
+ @param cpu user input cpu, which is the key of cpu_map
+ @ip ip user input ip, which is the key of ip_map
+ */
+void putResult(std::string &result, bool &found, const unsigned long time_lower, const unsigned long time_upper, OuterMap cpu_map, const int cpu, const std::string ip){
+    struct tm time_info;
+    OuterMap::iterator out = cpu_map.find(cpu);
+    //traversing the outer map, with cpu being the key, and ip map being the value
+    if(out != cpu_map.end()){
+        MiddleMap::iterator mid = out->second.find(ip);
+        //traversing the middle map, with ip being the key, and timestamp map being the value
+        if(mid != out->second.end()){
+            for(unsigned long i = time_lower; i <= time_upper; i+=60){
+                //traversing the inner map, with timestamp being the key, and usage being the value
+                InnerMap::iterator in = mid->second.find(i);
+                if(in != mid->second.end()){
+                    //converting the format of user's input time to timestamp so that it can be used as a key
+                    time_t t = i;
+                    int usage = mid->second[i];
+                    time_info = *gmtime(&t);
+                    if(found == true){
+                        result += ", ";
+                    }
+                    found = true;
+                    result = result + "(" + std::to_string(time_info.tm_year + 1900) + "-" + std::to_string(time_info.tm_mon + 1) + "-" + std::to_string(time_info.tm_mday) + " ";
+                    std::string hr = std::to_string(time_info.tm_hour);
+                    std::string m = std::to_string(time_info.tm_min);
+ 
+                    //makes sure min and hour always have 2 digits
+                    if(hr.length() == 1){
+                        hr = "0" + hr;
+                    }
+                    if(m.length() == 1){
+                        m = "0" + m;
+                    }
+                    result = result + hr + ":" + m + ", ";
+                    result = result + std::to_string(usage) + "%)";
+                }
+            }
+        }
+    }
+ }
+
 int main(int argc, const char * argv[]) {
     
     if(argc != 2){
@@ -107,6 +289,10 @@ int main(int argc, const char * argv[]) {
     
     
     std::string content;
+    unsigned long time_lower;
+    unsigned long time_upper;
+    int cpu;
+    std::string ip;
     
     std::string destination(argv[1]);
     
@@ -135,55 +321,17 @@ int main(int argc, const char * argv[]) {
             continue;
         }
         
-        std::ifstream ifs(full_file_name.c_str());
-        std::string line;
-        
-        while (std::getline(ifs, line)){
-            if(ifs.eof()){
-                break;
-            }
-            std::vector<std::string> strings;
-            strings = split_string(line, " ");
-            unsigned long timestamp;
-            timestamp = stoul(strings[0]);
-            std::string ip = strings[1];
-            int cpu;
-            cpu = stoi(strings[2]);
-            int usage;
-            usage = stoi(strings[3]);
-            
-            OuterMap::iterator out = cpu_map.find(cpu);
-            if (out != cpu_map.end())
-            {
-                MiddleMap::iterator mid = out->second.find(ip);
-                if (mid != out->second.end())
-                {
-                    mid->second[timestamp] = usage;
-                }
-                else
-                {
-                    InnerMap ts_map;
-                    ts_map[timestamp] = usage;
-                    out->second[ip] = ts_map;
-                }
-            }
-            else
-            {
-                InnerMap ts_map;
-                ts_map[timestamp] = usage;
-                MiddleMap ip_map;
-                ip_map[ip] = ts_map;
-                cpu_map[cpu] = ip_map;
-            }
-            
-            line.clear();
-        }
+        mapSetup(full_file_name, cpu_map);
+
         
         std::string user_input;
         
         while(true){
             std::string user_input;
             std::cout << "Program ready for QUERY." << std::endl;
+            std::cout << "Examples of correct commands are: " << std::endl;
+            std::cout << " > QUERY 192.168.1.10 1 2014-10-31 00:00 2014-10-31 00:05 " << std::endl;
+            std::cout << " > EXIT" << std::endl;
             std::getline(std::cin, user_input);
             
             std::string result = "CPU";
@@ -230,43 +378,7 @@ int main(int argc, const char * argv[]) {
                     continue;
                 }
                 
-                
-                //converting from time format of user input to timestamp
-                struct tm time_info;
-                std::string year = strings[3].substr(0,4);
-                std::string mon = strings[3].substr(5,2);
-                std::string mday = strings[3].substr(8,2);
-                std::string hour = strings[4].substr(0,2);
-                std::string min = strings[4].substr(3,2);
-                
-                time_info.tm_year = stoi(year);
-                time_info.tm_year = time_info.tm_year - 1900;
-                time_info.tm_mon = stoi(mon);
-                time_info.tm_mon = time_info.tm_mon - 1;
-                time_info.tm_mday = stoi(mday);
-                time_info.tm_hour = stoi(hour);
-                time_info.tm_min = stoi(min);
-                time_info.tm_sec = 0;
-                
-                //start time of request
-                time_t time_lower = timegm(&time_info);
-                
-                year = strings[5].substr(0,4);
-                mon = strings[5].substr(5,2);
-                mday = strings[5].substr(8,2);
-                hour = strings[6].substr(0,2);
-                min = strings[6].substr(3,2);
-                
-                time_info.tm_year = stoi(year);
-                time_info.tm_year = time_info.tm_year - 1900;
-                time_info.tm_mon = stoi(mon);
-                time_info.tm_mon = time_info.tm_mon - 1;
-                time_info.tm_mday = stoi(mday);
-                time_info.tm_hour = stoi(hour);
-                time_info.tm_min = stoi(min);
-                
-                //end time of request
-                time_t time_upper = timegm(&time_info);
+                commandParse(strings, time_lower, time_upper, cpu, ip);
                 
                 
                 int cpu;
@@ -275,47 +387,11 @@ int main(int argc, const char * argv[]) {
                 
                 OuterMap::iterator out = cpu_map.find(cpu);
                 result = result + std::to_string(cpu) + " usage on " + ip + ": \n";
-                
-                /*only if we have any result will we want to print it. found tells if there' any result available. Notice it also helps decide whether or not to put the comma in between results */
+
                 bool found = false;
                 
-                /*traversing the outer map, with cpu being the key, and ip map being the value*/
-                if(out != cpu_map.end()){
-                    MiddleMap::iterator mid = out->second.find(ip);
-                    
-                    /*traversing the middle map, with ip being the key, and timestamp map being the value*/
-                    if(mid != out->second.end()){
-                        for(unsigned long i = static_cast<unsigned long>(time_lower); i <= static_cast<unsigned long>(time_upper); i+=60){
-                            
-                            /*traversing the inner map, with timestamp being the key, and usage being the value*/
-                            InnerMap::iterator in = mid->second.find(i);
-                            if(in != mid->second.end()){
-                                
-                                /*converting the format of user's input time to timestamp so that it can be used as a key*/
-                                time_t t = i;
-                                int usage = mid->second[i];
-                                time_info = *gmtime(&t);
-                                if(found == true){
-                                    result += ", ";
-                                }
-                                found = true;
-                                result = result + "(" + std::to_string(time_info.tm_year + 1900) + "-" + std::to_string(time_info.tm_mon + 1) + "-" + std::to_string(time_info.tm_mday) + " ";
-                                std::string hr = std::to_string(time_info.tm_hour);
-                                std::string m = std::to_string(time_info.tm_min);
-                                
-                                /*makes sure min and hour always have 2 digits*/
-                                if(hr.length() == 1){
-                                    hr = "0" + hr;
-                                }
-                                if(m.length() == 1){
-                                    m = "0" + m;
-                                }
-                                result = result + hr + ":" + m + ", ";
-                                result = result + std::to_string(usage) + "%)";
-                            }
-                        }
-                    }
-                }
+                putResult(result, found, time_lower, time_upper, cpu_map, cpu, ip);
+
                 if(found == true){
                     std::cout << result << std::endl;
                 }
